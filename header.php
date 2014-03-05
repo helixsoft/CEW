99d19b680883f26c33f41a7343e903c251a347b5
Helixsoft <tushardharaster@gmail.com>
2014-03-05 11:32:16 +0530

    add css framework

--- header.php
+++ header.php
@@ -4,7 +4,10 @@
         <meta charset="<?php bloginfo( 'charset' ); ?>">
         <title><?php wp_title( '|', true, 'right' ); ?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">       
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+        <link rel="stylesheet" href="<?php echo THEMEROOT?>/css/base.css">
+        <link rel="stylesheet" href="<?php echo THEMEROOT?>/css/skeleton.css">
+        <link rel="stylesheet" href="<?php echo THEMEROOT?>/css/layout.css">       
         <link rel="stylesheet" href="<?php echo THEMEROOT?>/style.css" media="all">
         <!--[if lt IE 9]>
             <script src="<?php echo THEMEROOT?>/js/html5.js"></script>
