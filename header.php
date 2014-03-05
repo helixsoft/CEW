48a51d49138d16037664f19da5e9ec326be568b4
Helixsoft <tushardharaster@gmail.com>
2014-03-05 10:50:43 +0530

    add header html and footer html

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <title><?php wp_title( '|', true, 'right' ); ?></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
+        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">       
+        <link rel="stylesheet" href="<?php echo THEMEROOT?>/style.css" media="all">
+        <!--[if lt IE 9]>
+            <script src="<?php echo THEMEROOT?>/js/html5.js"></script>
+            <![endif]-->
+        <script src="<?php echo THEMEROOT?>/js/jquery.min.js"></script>
+        <script src="<?php echo THEMEROOT?>/js/modernizr.js"></script>
+        <?php wp_head();?>
+    </head>
+    <body>
