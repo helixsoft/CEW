f5742b1b08d4a643f3eac4ea3e7e3367d3ba60ce
Helixsoft <tushardharaster@gmail.com>
2014-03-14 17:15:44 +0530

    cew values complete

--- page-policy.php
+++ page-policy.php
@@ -19,15 +19,15 @@ get_header(); ?>
 				</div>
 				<div class="cew-value-item">
 					<h2>
-						<span></span>
-						<span>CEW PEOPLE</span>
+						<span class="pawn"></span>
+						<span class="text">CEW PEOPLE</span>
 					</h2>
 					<p>We attract and recruit the finest people. Our organization is built by people from within. For this we promote and reward people according to only their performance and nothing else. We know very well that our people will always be our most important asset.</p>
 				</div>
 				<div class="cew-value-item">
 					<h2>
-						<span></span>
-						<span>Leadership</span>
+						<span class="queen"></span>
+						<span class="text">Leadership</span>
 					</h2>
 					<p>
 						We all have a clear vision of our responsibilities, objectives and goals. We focus our resources to achieve leadership objectives and strategies. We develop the capability to apply our strategies and eliminate organizational barriers.. We are all leaders in our area of responsibility with a deep commitment to obtain leadership results. 
@@ -35,8 +35,8 @@ get_header(); ?>
 				</div>
 				<div class="cew-value-item">
 					<h2>
-						<span></span>
-						<span>Ownership</span>
+						<span class="king"></span>
+						<span class="text">Ownership</span>
 					</h2>
 					<p>
 						We all are personally responsible to meet the business requirements, improve our systems and help others improve their effectiveness. We all act like owners treating the Company's assets as our own and behaving with the Company's long term success in mind.
@@ -44,8 +44,8 @@ get_header(); ?>
 				</div>
 				<div class="cew-value-item">
 					<h2>
-						<span></span>
-						<span>Passion for Wining</span>
+						<span class="knight"></span>
+						<span class="text">Passion for Wining</span>
 					</h2>
 					<p>
 						We are determined to be the best at doing what matters most. We have a healthy dissatisfaction with the status quo. We have a compelling desire to improve and to win in the marketplace.
@@ -53,14 +53,23 @@ get_header(); ?>
 				</div>
 				<div class="cew-value-item">
 					<h2>
-						<span></span>
-						<span>Trust</span>
+						<span class="bishop"></span>
+						<span class="text">Trust</span>
 					</h2>
 					<p>
 						We respect our CEW colleagues, customers, suppliers, subcontractors and treat them as we want to be treated. We have confidence in each other's capabilities and intentions. We believe that people work best when there is a foundation of trust.
 					</p>
 				</div>
 			</div>
+		</div>
+	</div>
+	<div class="container">
+		<div class="sixteen columns">
+		</div>
+	</div>
+	<hr/>
+	<div class="container">
+		<div class="sixteen columns">
 		</div>
 	</div>
 </section>
